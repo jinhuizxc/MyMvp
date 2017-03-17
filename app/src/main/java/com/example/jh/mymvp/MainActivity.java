ab5e4fee4470cb0d3a2536d47920c4d28497664a
jinhuizxc <1004260403@qq.com>
2017-03-17 09:48:24 +0800

    提交修改项

--- app/src/main/java/com/example/jh/mymvp/MainActivity.java
+++ app/src/main/java/com/example/jh/mymvp/MainActivity.java
@@ -2,29 +2,39 @@ package com.example.jh.mymvp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.jh.mymvp.presenter.ShowPresenter;
 import com.example.jh.mymvp.presenter.impl.ShowPresenterImpl;
 import com.example.jh.mymvp.view.ShowView;
 
-public class MainActivity extends AppCompatActivity implements ShowView{
+public class MainActivity extends AppCompatActivity implements ShowView, View.OnClickListener {
 
     private TextView textView;
+    private Button button;
     private ShowPresenter showPresenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
         showPresenter = new ShowPresenterImpl(this);
         //这个语句逻辑可以根据需求点击按钮或者其他操作完成之后进行view的实现
-        showPresenter.show();
+//        showPresenter.show();
     }
 
     //界面显示view
     @Override
     public void show(String str) {
         textView.setText(str);
+    }
+
+    @Override
+    public void onClick(View v) {
+        showPresenter.show();
     }
 }
